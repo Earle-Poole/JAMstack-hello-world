de365a5cd03c16b4afe1c964cf577ab153c674bf
earle-poole <eepoole4@gmail.com>
2018-10-31 23:41:47 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hello!</h1>
+    <p>My name is Earle Poole, and I'm excited to get hacking.</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
